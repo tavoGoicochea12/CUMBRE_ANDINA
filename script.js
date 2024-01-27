4358f817f539ee344ba25ea1522b7eaa3f95ca26
Bahuro <bautistahuingojuanroimer@gmail.com>
2024-01-26 23:39:44 -0500

    "ordenado index, style,script "

---
+++ script.js
@@ -0,0 +1,9 @@
+function moverIzquierda() {
+    // Lógica para mover hacia la izquierda
+    console.log('Mover hacia la izquierda');
+}
+
+function moverDerecha() {
+    // Lógica para mover hacia la derecha
+    console.log('Mover hacia la derecha');
+}
